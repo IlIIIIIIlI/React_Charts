2e6119dca1907d4e60f4907b5f15b10bdcdd5b15
iliiiiiili <yangquechen@gmail.com>
2022-07-31 17:31:17 +1000

    some charts

--- frontchart/components/Layout.js
+++ frontchart/components/Layout.js
@@ -1,13 +1,21 @@
-
 import styles from "../styles/Home.module.css"
+import Meta from "./Meta"
+import Navigationbar from '../components/Navigationbar'
+
 
 const Layout = ({ children }) => {
+
     return (
         <>
+            <Meta title="房地产透视" />
+            <Navigationbar />
             <div className={styles.container}>
-                <div className={style.main}></div>
-                {children}
+                <div className={styles.main}>
+                    {children}
+                </div>
             </div>
         </>
     )
 }
+
+export default Layout
--- frontchart/pages/index.js
+++ frontchart/pages/index.js
@@ -1,11 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css"
+import { useRouter } from 'next/router'
+import DoughnutChart from "../components/LineChart"
+import ChartGrid from "../components/ChartGrid"
 
-export default function Home() {
+
+
+export default function Home({ data }) {
+
+  console.log(data)
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>房产数据透视</h3>
+      <h3 className={styles.title}>房地产透视</h3>
+      <ChartGrid data={data} />
     </div>
   )
+}
+
+// If you export a function called getServerSideProps (Server-Side Rendering) 
+// from a page, Next.js will pre-render this page on each request using the 
+// data returned by getServerSideProps.
+// read api
+export const getServerSideProps = async () => {
+  const res = await fetch("http://localhost:8000")
+  const data = await res.json()
+  return {
+    props: {
+      data
+    }
+  }
 }
